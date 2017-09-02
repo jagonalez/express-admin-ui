b3f7edb8666b517f07009321711c4dd073f538c9
Jeremy Gonzalez <jeremygonzalez@outlook.com>
2017-09-02 11:16:07 -0700

    add testing to collections

--- test/collections.spec.js
+++ test/collections.spec.js
@@ -2,14 +2,24 @@
 
 const collections = require('../lib/collections.js')
 const expect = require('chai').expect
-const express = require('express')
+const utils = require('../lib/utils.js')
+const helper = require('./helper')
 
 describe('Collections module', () => {
+  beforeEach(() => {
+    utils.setCollection(helper.collection)
+  })
+
+  afterEach(() => {
+    utils.setCollection([])
+  })
+  var router = collections()
+
   it('should export a router function', () => {
     expect(collections).to.be.a('function')
   })
   describe('routes', () => {
-    var router = collections()
+
     var route = router.stack[0].route
     it('should contain 1 route', () => {
       expect(router.stack.length).to.equal(1)
@@ -17,5 +27,9 @@ describe('Collections module', () => {
     it('should have a get method', () => {
       expect(router.stack[0].route.methods.get).to.equal(true)
     })
+  })
+  it('should return a view when get is called', () => {
+    router.stack[0].route.stack[0].handle(helper.request, helper.response)
+    expect(helper.response.data).to.contain('</html>')
   })
 })
