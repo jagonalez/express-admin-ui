a6bfba2d8e91f3a18323bc64cf5873e5c7f6a200
Jeremy Gonzalez <jeremygonzalez@outlook.com>
2017-09-02 10:12:45 -0700

    add util unit tests

---
+++ test/utils.spec.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const utils = require('../lib/utils.js')
+const expect = require('chai').expect
+
+describe('Utils module', () => {
+  describe('"DB"', () => {
+    it('should export an object', () => {
+      expect(utils).to.be.an('object')
+    })
+    it('should have a db property', () => {
+      expect(utils.db).to.be.null
+    })
+    it('should have a collection property', () => {
+      expect(utils.collection).to.be.empty
+    })
+    describe('setDB', () => {
+      it ('should set the db context', () => {
+        var db = {set: true}
+        utils.setDB(db)
+        expect(utils.db).to.equal(db)
+      })
+    })
+    describe('setCollection', () => {
+      it('should set the collection', () => {
+        var collection = [1,2,3]
+        utils.setCollection(collection)
+        expect(utils.collection).to.equal(collection)
+      })
+    })
+  })
+})
